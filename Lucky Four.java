8e63dbb3a16efad88e1cd907ba77a79ed759a2e4
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2021-12-01 22:15:31 +0530

    Lucky Four | Problem Code: LUCKFOUR

--- Lucky Four.java
+++ Lucky Four.java
@@ -1,1 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- >0)
+		{
+		    int rev=0;
+		    int n = sc.nextInt();
+		    while(n>0){
+		        rev = rev*10 + (n%10);
+		        n/=10;
+		    }
+		    System.out.println(rev);
+		}
+	}
+}
